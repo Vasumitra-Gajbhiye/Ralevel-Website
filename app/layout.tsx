a51a7bf97c4caf1ce68fe2473e22749dcc737225
vasu <vasumitragajbhiye20@gmail.com>
2026-04-08 09:32:56 +0530

    added official nextjs google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
-import GoogleAnalyticsTracker from "@/components/GoogleAnalyticsTracker";
+import { GoogleAnalytics } from "@next/third-parties/google"; // <-- Official Package
 import { Analytics } from "@vercel/analytics/next";
 import { Poppins } from "next/font/google";
-import Script from "next/script";
-import { Suspense } from "react";
 import { Toaster } from "sonner";
 
 import "./globals.css";
@@ -41,68 +39,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Ezoic Privacy Scripts - MUST BE FIRST */}
-        {/* <Script
-          id="ezoic-privacy-1"
-          strategy="beforeInteractive"
-          data-cfasync="false"
-          src="https://cmp.gatekeeperconsent.com/min.js"
-        />
-
-        <Script
-          id="ezoic-privacy-2"
-          strategy="beforeInteractive"
-          data-cfasync="false"
-          src="https://the.gatekeeperconsent.com/cmp.min.js"
-        /> */}
-
-        {/* Ezoic Header Script */}
-        {/* <Script
-          id="ezoic-header"
-          strategy="beforeInteractive"
-          async
-          src="//www.ezojs.com/ezoic/sa.min.js"
-        />
-
-        <Script id="ezoic-init" strategy="beforeInteractive">
-          {`
-            window.ezstandalone = window.ezstandalone || {};
-            ezstandalone.cmd = ezstandalone.cmd || [];
-          `}
-        </Script>
-
-        <Script
-          id="ezoic-analytics"
-          strategy="beforeInteractive"
-          src="//ezoicanalytics.com/analytics.js"
-        /> */}
-        {/* Google Analytics */}
-        <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=G-8WQ1YNJQR8`}
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-8WQ1YNJQR8', {
-            page_path: window.location.pathname,
-          });
-        `}
-        </Script>
         <link rel="icon" href="/favicon.png" />
       </head>
       <body className={poppins.className + " tracking-widest	"}>
-        <Suspense fallback={null}>
+        {/* <Suspense fallback={null}>
           <GoogleAnalyticsTracker />
-        </Suspense>
+        </Suspense> */}
         {/* <Navigation /> */}
         {children}
         <Toaster richColors position="top-right" />
         <Analytics />
         {/* <ContactUs /> */}
       </body>
+      <GoogleAnalytics gaId="G-8WQ1YNJQR8" />
     </html>
   );
 }
